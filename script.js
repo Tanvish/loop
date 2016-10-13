fc13edd5a8f2593e8782c2e0cfee18a78d58a0b6
Tanvish jha <Tanvish@users.noreply.github.com>
2016-10-13 12:27:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var a,b,c,d;
+function yo1(){
+var foo = prompt("Give me input");
+if (foo=="cool"){
+	var x = document.getElementById('ran1');
+   x.style.background = 'green';
+   x.style.color="white";
+}
+else{
+var x = document.getElementById('ran1');
+   x.style.background = 'red';
+   x.style.color="white";
+}
+}
